fb94de9814a8359738b3924cb6a405b232102e5c
JaanKaru <user@SDAcourse.pl>
2024-09-23 22:23:21 +0300

    use config to store django key

--- orbital_bookstore/settings.py
+++ orbital_bookstore/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-!@vjay^t0nybs-g3rdfwb^&_xo@_8h$hn_812lk_tptbzg62(8"
+SECRET_KEY = config('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
