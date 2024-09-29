7555a79fa945217fe562e597cc1ea5dd8b2c9031
JaanKaru <user@SDAcourse.pl>
2024-09-29 18:55:37 +0300

    new dropdown menu and more

--- store/models.py
+++ store/models.py
@@ -52,6 +52,94 @@ class Book(models.Model):
         return self.title
 
 
+class Ebook(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    cover_art = models.ImageField(upload_to='ebooks/', blank=True, null=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    availability = models.BooleanField(default=True)
+    download_link = models.URLField(max_length=400)
+
+    def __str__(self):
+        return self.title
+
+
+    # Accessory model
+class Accessory(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    availability = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
+
+
+# Book Wraps as a subcategory of Accessories
+class BookWrap(models.Model):
+    accessory = models.OneToOneField(Accessory, on_delete=models.CASCADE)
+    color = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    availability = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f"Book Wrap - {self.accessory.name}"
+
+
+# Exlibris as a subcategory of Accessories
+class Exlibris(models.Model):
+    accessory = models.OneToOneField(Accessory, on_delete=models.CASCADE)
+    design = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    availability = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f"Exlibris - {self.accessory.name}"
+
+    # School and Office Supplies model
+class SchoolOffice(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    availability = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
+
+    # Booklets/Folders as a subcategory of School and Office
+class BookletFolder(models.Model):
+    school_office = models.OneToOneField(SchoolOffice, on_delete=models.CASCADE)
+    size = models.CharField(max_length=50)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    availability = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f"Booklet/Folder - {self.school_office.name}"
+
+
+    # Pencils as a subcategory of School and Office
+class Pencil(models.Model):
+    school_office = models.OneToOneField(SchoolOffice, on_delete=models.CASCADE)
+    pencil_type = models.CharField(max_length=50)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    availability = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f"Pencil - {self.school_office.name}"
+
+
+    # Other supplies as a subcategory of School and Office
+class Other(models.Model):
+    school_office = models.OneToOneField(SchoolOffice, on_delete=models.CASCADE)
+    description = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    availability = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f"Other - {self.school_office.name}"
+
+
 class Review(models.Model):
     book = models.ForeignKey(Book, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -68,7 +156,15 @@ class Favorite(models.Model):
 
 class Cart(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE, blank=True, null=True)
+    ebook = models.ForeignKey(Ebook, on_delete=models.CASCADE, blank=True, null=True)
+    accessory = models.ForeignKey(Accessory, on_delete=models.CASCADE, blank=True, null=True)
+    pencil = models.ForeignKey(Pencil, on_delete=models.CASCADE, blank=True, null=True)
+    other = models.ForeignKey(Other, on_delete=models.CASCADE, blank=True, null=True)
+    book_wrap = models.ForeignKey(BookWrap, on_delete=models.CASCADE, blank=True, null=True)
+    exlibris = models.ForeignKey(Exlibris, on_delete=models.CASCADE, blank=True, null=True)
+    booklet_folder = models.ForeignKey(BookletFolder, on_delete=models.CASCADE, blank=True, null=True)
+    school_office = models.ForeignKey(SchoolOffice, on_delete=models.CASCADE, blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     quantity = models.IntegerField(default=1)
     total_cost = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
@@ -77,7 +173,18 @@ class Cart(models.Model):
 class Order(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     order_date = models.DateTimeField(auto_now_add=True)
-    book = models.ForeignKey(Book, on_delete=models.CASCADE, default=1)
+
+    # Add fields for all item types
+    book = models.ForeignKey(Book, on_delete=models.CASCADE, blank=True, null=True)
+    ebook = models.ForeignKey(Ebook, on_delete=models.CASCADE, blank=True, null=True)
+    accessory = models.ForeignKey(Accessory, on_delete=models.CASCADE, blank=True, null=True)
+    pencil = models.ForeignKey(Pencil, on_delete=models.CASCADE, blank=True, null=True)
+    other = models.ForeignKey(Other, on_delete=models.CASCADE, blank=True, null=True)
+    book_wrap = models.ForeignKey(BookWrap, on_delete=models.CASCADE, blank=True, null=True)
+    exlibris = models.ForeignKey(Exlibris, on_delete=models.CASCADE, blank=True, null=True)
+    booklet_folder = models.ForeignKey(BookletFolder, on_delete=models.CASCADE, blank=True, null=True)
+    school_office = models.ForeignKey(SchoolOffice, on_delete=models.CASCADE, blank=True, null=True)
+
     price = models.DecimalField(max_digits=10, decimal_places=2)
     quantity = models.IntegerField()
     total_cost = models.DecimalField(max_digits=10, decimal_places=2)
@@ -98,3 +205,6 @@ class OrderItem(models.Model):
 
     def __str__(self):
         return f'{self.book.title} - {self.order.user.username}'
+
+
+
--- store/views.py
+++ store/views.py
@@ -1,22 +1,21 @@
 import logging
 from django.db.models import Q
 from django.shortcuts import render
-from .models import Book, Category, Cart, Order, Favorite, Review, OrderItem
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from .models import User
 from django.shortcuts import get_object_or_404, redirect
-from django import forms
 from django.contrib.auth import login
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import get_user_model
 from django.contrib.auth.hashers import make_password
+from .models import Book, Ebook, Accessory, BookWrap, Exlibris, SchoolOffice, BookletFolder, Pencil, Other, Cart, Order, Favorite, Review
+
+
 
 
 def home(request):
-    books = Book.objects.all()
     cart_items_count = Cart.objects.filter(user=request.user).count() if request.user.is_authenticated else 0
-    return render(request, 'store/home.html', {'books': books, 'cart_items_count': cart_items_count})
+    return render(request, 'store/home.html', {'cart_items_count': cart_items_count})
 
 
 def book_detail(request, book_id):
@@ -65,20 +64,61 @@ def register(request):
 
 
 @login_required
-def add_to_cart(request, book_id):
-    book = get_object_or_404(Book, id=book_id)
+def add_to_cart(request, item_id):
+    # Fetch items from all possible models
+    book = Book.objects.filter(id=item_id).first()
+    ebook = Ebook.objects.filter(id=item_id).first()
+    accessory = Accessory.objects.filter(id=item_id).first()
+    pencil = Pencil.objects.filter(id=item_id).first()
+    other = Other.objects.filter(id=item_id).first()
+    book_wrap = BookWrap.objects.filter(id=item_id).first()
+    exlibris = Exlibris.objects.filter(id=item_id).first()
+    booklet_folder = BookletFolder.objects.filter(id=item_id).first()
+    school_office = SchoolOffice.objects.filter(id=item_id).first()
 
-    cart_item, created = Cart.objects.get_or_create(user=request.user, book=book)
+    # Check and add item to cart
+    if book:
+        cart_item, created = Cart.objects.get_or_create(user=request.user, book=book)
+        item_price = book.price
+    elif ebook:
+        cart_item, created = Cart.objects.get_or_create(user=request.user, ebook=ebook)
+        item_price = ebook.price
+    elif accessory:
+        cart_item, created = Cart.objects.get_or_create(user=request.user, accessory=accessory)
+        item_price = accessory.price
+    elif school_office:
+        cart_item, created = Cart.objects.get_or_create(user=request.user, school_office=school_office)
+        item_price = school_office.price
+    elif pencil:
+        cart_item, created = Cart.objects.get_or_create(user=request.user, pencil=pencil)
+        item_price = pencil.price
+    elif other:
+        cart_item, created = Cart.objects.get_or_create(user=request.user, other=other)
+        item_price = other.price
+    elif book_wrap:
+        cart_item, created = Cart.objects.get_or_create(user=request.user, book_wrap=book_wrap)
+        item_price = book_wrap.price
+    elif exlibris:
+        cart_item, created = Cart.objects.get_or_create(user=request.user, exlibris=exlibris)
+        item_price = exlibris.price
+    elif booklet_folder:
+        cart_item, created = Cart.objects.get_or_create(user=request.user, booklet_folder=booklet_folder)
+        item_price = booklet_folder.price
+    else:
+        messages.error(request, "Item not found.")
+        return redirect('home')
 
+    # Update quantity and total cost
     if created:
         cart_item.quantity = 1
-        cart_item.total_cost = book.price
+        cart_item.total_cost = item_price
     else:
         cart_item.quantity += 1
-        cart_item.total_cost = cart_item.quantity * book.price
+        cart_item.total_cost = cart_item.quantity * item_price
 
     cart_item.save()
     return redirect('view_cart')
+
 
 
 # Viewing the cart
@@ -101,14 +141,25 @@ def remove_from_cart(request, item_id):
 @login_required
 def checkout(request):
     cart_items = Cart.objects.filter(user=request.user)
-    total_price = sum(item.book.price * item.quantity for item in cart_items)
+
+    # Update total price calculation to check all item types
+    total_price = sum(
+        (item.book.price if item.book else 0) +
+        (item.ebook.price if item.ebook else 0) +
+        (item.accessory.price if item.accessory else 0) +
+        (item.pencil.price if item.pencil else 0) +
+        (item.other.price if item.other else 0) +
+        (item.book_wrap.price if item.book_wrap else 0) +
+        (item.exlibris.price if item.exlibris else 0) +
+        (item.booklet_folder.price if item.booklet_folder else 0) +
+        (item.school_office.price if item.school_office else 0)
+        for item in cart_items
+    )
 
     if request.method == 'POST':
-        # Get the payment method from the form
         payment_method = request.POST.get('payment_method')
         delivery_email = request.POST.get('email')
 
-        # Check if payment method is provided
         if not payment_method:
             return render(request, 'store/checkout.html', {
                 'cart_items': cart_items,
@@ -116,21 +167,42 @@ def checkout(request):
                 'error_message': 'Please select a payment method.'
             })
 
-        # Create the orders
         for item in cart_items:
-            Order.objects.create(
-                user=request.user,
-                book=item.book,
-                price=item.book.price,
-                quantity=item.quantity,
-                total_cost=item.book.price * item.quantity,
-                status='Pending',
-                payment_method=payment_method,  # Pass the payment method here
-                payment_status='Unpaid',
-                delivery_email=delivery_email
-            )
+            order_data = {
+                'user': request.user,
+                'price': item.total_cost / item.quantity,  # Use per-item price
+                'quantity': item.quantity,
+                'total_cost': item.total_cost,
+                'status': 'Pending',
+                'payment_method': payment_method,
+                'payment_status': 'Unpaid',
+                'delivery_email': delivery_email,
+            }
 
-        cart_items.delete()  # Clear the cart after order is created
+            # Create an Order for the correct item type
+            if item.book:
+                order_data['book'] = item.book
+            elif item.ebook:
+                order_data['ebook'] = item.ebook
+            elif item.accessory:
+                order_data['accessory'] = item.accessory
+            elif item.pencil:
+                order_data['pencil'] = item.pencil
+            elif item.other:
+                order_data['other'] = item.other
+            elif item.book_wrap:
+                order_data['book_wrap'] = item.book_wrap
+            elif item.exlibris:
+                order_data['exlibris'] = item.exlibris
+            elif item.booklet_folder:
+                order_data['booklet_folder'] = item.booklet_folder
+            elif item.school_office:
+                order_data['school_office'] = item.school_office
+
+            # Create the Order
+            Order.objects.create(**order_data)
+
+        cart_items.delete()  # Clear the cart after order creation
         return redirect('order_success')
 
     return render(request, 'store/checkout.html', {'cart_items': cart_items, 'total_price': total_price})
@@ -144,20 +216,29 @@ def order_success(request):
 
 
 @login_required
-def add_to_favorites(request, book_id):
-    book = get_object_or_404(Book, id=book_id)
-    favorite, created = Favorite.objects.get_or_create(
-        user=request.user,
-        book=book,
-        defaults={'fav_rating': 1}
-    )
+def add_to_favorites(request, item_id):
+    # Fetch items from all possible models
+    book = Book.objects.filter(id=item_id).first()
+    ebook = Ebook.objects.filter(id=item_id).first()
+    accessory = Accessory.objects.filter(id=item_id).first()
+
+    # Check and add the item to favorites
+    if book:
+        favorite, created = Favorite.objects.get_or_create(user=request.user, book=book)
+    elif ebook:
+        favorite, created = Favorite.objects.get_or_create(user=request.user, ebook=ebook)
+    elif accessory:
+        favorite, created = Favorite.objects.get_or_create(user=request.user, accessory=accessory)
+    else:
+        messages.error(request, "Item not found.")
+        return redirect('home')
 
     if created:
-        messages.success(request, f'{book.title} has been added to your favorites.')
+        messages.success(request, 'Item added to your favorites.')
     else:
-        messages.info(request, f'{book.title} is already in your favorites.')
+        messages.info(request, 'Item is already in your favorites.')
 
-    return redirect('book_detail', book_id=book_id)
+    return redirect('home')
 
 
 @login_required
@@ -195,6 +276,7 @@ def view_reviews(request):
 def update_cart_quantity(request, item_id, operation):
     cart_item = get_object_or_404(Cart, id=item_id, user=request.user)
 
+    # Handle the quantity increase/decrease
     if operation == 'increase':
         cart_item.quantity += 1
     elif operation == 'decrease' and cart_item.quantity > 1:
@@ -204,33 +286,147 @@ def update_cart_quantity(request, item_id, operation):
         cart_item.delete()
         return redirect('view_cart')
 
-    cart_item.total_cost = cart_item.quantity * cart_item.book.price
-    cart_item.save()
+    # Update the total cost based on the item type
+    if cart_item.book:
+        cart_item.total_cost = cart_item.quantity * cart_item.book.price
+    elif cart_item.ebook:
+        cart_item.total_cost = cart_item.quantity * cart_item.ebook.price
+    elif cart_item.accessory:
+        cart_item.total_cost = cart_item.quantity * cart_item.accessory.price
+    elif cart_item.pencil:
+        cart_item.total_cost = cart_item.quantity * cart_item.pencil.price
+    elif cart_item.other:
+        cart_item.total_cost = cart_item.quantity * cart_item.other.price
+    elif cart_item.book_wrap:
+        cart_item.total_cost = cart_item.quantity * cart_item.book_wrap.price
+    elif cart_item.exlibris:
+        cart_item.total_cost = cart_item.quantity * cart_item.exlibris.price
+    elif cart_item.booklet_folder:
+        cart_item.total_cost = cart_item.quantity * cart_item.booklet_folder.price
+    elif cart_item.school_office:
+        cart_item.total_cost = cart_item.quantity * cart_item.school_office.price
+    else:
+        cart_item.total_cost = 0  # Set a default value or handle the error
 
+    cart_item.save()
     return redirect('view_cart')
 
 
 def search(request):
     query = request.GET.get('q', '')
+
     if query:
+        # Fetching books, ebooks, and other items based on query
         books = Book.objects.filter(
             Q(title__icontains=query) |
             Q(author__icontains=query) |
             Q(description__icontains=query)
         )
-    else:
-        books = Book.objects.none()
+        ebooks = Ebook.objects.filter(
+            Q(title__icontains=query) |
+            Q(author__icontains=query) |
+            Q(description__icontains=query)
+        )
+        accessories = Accessory.objects.filter(
+            Q(name__icontains=query) |
+            Q(description__icontains=query)
+        )
+        book_wraps = BookWrap.objects.filter(
+            Q(accessory__name__icontains=query) |
+            Q(color__icontains=query)
+        )
+        booklets_folders = BookletFolder.objects.filter(
+            Q(school_office__name__icontains=query) |
+            Q(size__icontains=query)
+        )
+        exlibris = Exlibris.objects.filter(
+            Q(accessory__name__icontains=query) |
+            Q(design__icontains=query)
+        )
+        pencils = Pencil.objects.filter(
+            Q(school_office__name__icontains=query) |
+            Q(pencil_type__icontains=query)
+        )
+        school_and_office = SchoolOffice.objects.filter(
+            Q(name__icontains=query) |
+            Q(description__icontains=query)
+        )
 
-    return render(request, 'store/search_results.html', {'books': books})
+        # Combine results from all item types into one list
+        items = list(books) + list(ebooks) + list(accessories) + list(book_wraps) + \
+                list(school_and_office) + list(exlibris) + list(pencils) + list(booklets_folders)
+    else:
+        items = []
+
+    return render(request, 'store/search_results.html', {'items': items})
+
 
 def about_us(request):
     return render(request, 'store/about_us.html')
 
+
 def contact_us(request):
     return render(request, 'store/contact_us.html')
+
 
 def terms_and_conditions(request):
     return render(request, 'store/terms_and_conditions.html')
 
+
 def shipping_information(request):
     return render(request, 'store/shipping_information.html')
+
+
+def books(request):
+    return render(request, 'store/books.html')
+
+
+def ebooks(request):
+    return render(request, 'store/ebooks.html')
+
+
+def accessories(request):
+    return render(request, 'store/accessories.html')
+
+
+def book_wraps(request):
+    book_wraps = BookWrap.objects.all()  # Fetch all book wraps
+    return render(request, 'store/book_wraps.html', {'book_wraps': book_wraps})
+
+
+def exlibris(request):
+    exlibris_items = Exlibris.objects.all()  # Fetch all exlibris
+    return render(request, 'store/exlibris.html', {'exlibris_items': exlibris_items})
+
+
+def school_and_office(request):
+    return render(request, 'store/school_and_office.html')
+
+
+def booklets_folders(request):
+    booklets_folders = BookletFolder.objects.all()  # Fetch all booklets/folders
+    return render(request, 'store/booklets_folders.html', {'booklets_folders': booklets_folders})
+
+
+def pencils(request):
+    return render(request, 'store/pencils.html')
+
+
+def other(request):
+    return render(request, 'store/other.html')
+
+
+def books_view(request):
+    books = Book.objects.all()  # Fetch all books from the database
+    return render(request, 'store/books.html', {'books': books})
+
+
+# View for E-books page
+def ebooks_view(request):
+    ebooks = Ebook.objects.all()  # Fetch all e-books
+    return render(request, 'store/ebooks.html', {'ebooks': ebooks})
+
+
+def ebook_detail(request, ebook_id):
+    ebook = get_object_or_404(Ebook, id=ebook_id)  # Fetch the e-book or return a 404 error
+    return render(request, 'store/e-book_details.html', {'ebook': ebook})
