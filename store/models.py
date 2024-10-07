508a3f9aa7648a27174b74ed31b9b54508895a94
mellukas123 <merlynmey52@gmail.com>
2024-10-07 23:42:28 +0300

    Fixes

--- store/models.py
+++ store/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.core.validators import MinValueValidator, MaxValueValidator
 from django.conf import settings
+from django.utils import timezone
+
 
 STATUS_CHOICES = [
     ('pending', 'Pending'),
