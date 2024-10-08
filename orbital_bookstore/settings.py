775b973e4ab634309e9837f28722ccfa99499713
mellukas123 <merlynmey52@gmail.com>
2024-10-08 20:04:58 +0300

    Navbar styling

--- orbital_bookstore/settings.py
+++ orbital_bookstore/settings.py
@@ -119,8 +119,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    BASE_DIR / 'static',
+    BASE_DIR / "static",
 ]
+
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
 LOGIN_REDIRECT_URL = '/'
